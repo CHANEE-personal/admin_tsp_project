c7f6374dd33a6c7e9b1f48a9b2aca71a461ce365
CHO CHAN HEE <cksgml4424@gmail.com>
2022-05-09 23:09:39 +0900

    Admin 프로덕션 상세 테스트 케이스 추가

--- src/test/java/com/tsp/new_tsp_admin/api/production/service/AdminProductionJpaRepositoryTest.java
+++ src/test/java/com/tsp/new_tsp_admin/api/production/service/AdminProductionJpaRepositoryTest.java
@@ -1,7 +1,11 @@
 package com.tsp.new_tsp_admin.api.production.service;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.tsp.new_tsp_admin.api.domain.common.CommonImageDTO;
+import com.tsp.new_tsp_admin.api.domain.common.CommonImageEntity;
 import com.tsp.new_tsp_admin.api.domain.production.AdminProductionDTO;
+import com.tsp.new_tsp_admin.api.domain.production.AdminProductionEntity;
+import com.tsp.new_tsp_admin.api.model.mapper.ModelImageMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,11 +20,17 @@ import org.springframework.test.context.TestPropertySource;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+import static com.tsp.new_tsp_admin.api.domain.production.AdminProductionEntity.builder;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.mockito.BDDMockito.given;
 
 @DataJpaTest
 @Transactional
@@ -56,5 +66,124 @@ class AdminProductionJpaRepositoryTest {
 
         // then
         assertThat(productionList.size()).isGreaterThan(0);
+    }
+
+    @Test
+    public void 프로덕션상세조회테스트() throws Exception {
+
+        // given
+        AdminProductionEntity adminProductionEntity = builder().idx(1).build();
+
+        // when
+        AdminProductionDTO productionInfo = adminProductionJpaRepository.findOneProduction(adminProductionEntity);
+
+        assertAll(() -> assertThat(productionInfo.getIdx()).isEqualTo(1),
+                () -> {
+                    assertThat(productionInfo.getTitle()).isEqualTo("프로덕션 테스트");
+                    assertNotNull(productionInfo.getTitle());
+                },
+                () -> {
+                    assertThat(productionInfo.getDescription()).isEqualTo("프로덕션 테스트");
+                    assertNotNull(productionInfo.getDescription());
+                },
+                () -> {
+                    assertThat(productionInfo.getVisible()).isEqualTo("Y");
+                    assertNotNull(productionInfo.getVisible());
+                });
+
+        assertThat(productionInfo.getProductionImage().get(0).getTypeName()).isEqualTo("production");
+        assertThat(productionInfo.getProductionImage().get(0).getImageType()).isEqualTo("main");
+        assertThat(productionInfo.getProductionImage().get(0).getFileName()).isEqualTo("52d4fdc8-f109-408e-b243-85cc1be207c5.jpg");
+        assertThat(productionInfo.getProductionImage().get(0).getFileMask()).isEqualTo("1223024921206.jpg");
+    }
+
+    @Test
+    public void 프로덕션BDD조회테스트() throws Exception {
+
+        // given
+        ConcurrentHashMap<String, Object> productionMap = new ConcurrentHashMap<>();
+        productionMap.put("jpaStartPage", 1);
+        productionMap.put("size", 3);
+
+        CommonImageDTO commonImageDTO = CommonImageDTO.builder()
+                .idx(1)
+                .imageType("main")
+                .fileName("test.jpg")
+                .fileMask("test.jpg")
+                .filePath("/test/test.jpg")
+                .typeIdx(1)
+                .typeName("production")
+                .build();
+
+        List<CommonImageDTO> commonImageDtoList = new ArrayList<>();
+        commonImageDtoList.add(commonImageDTO);
+
+        List<AdminProductionDTO> productionList = new ArrayList<>();
+        productionList.add(AdminProductionDTO.builder().idx(1).title("프로덕션 테스트")
+                .description("프로덕션 테스트").productionImage(commonImageDtoList).build());
+
+        given(mockAdminProductionJpaRepository.findProductionsList(productionMap)).willReturn(productionList);
+
+        // when
+        Integer idx = mockAdminProductionJpaRepository.findProductionsList(productionMap).get(0).getIdx();
+        String title = mockAdminProductionJpaRepository.findProductionsList(productionMap).get(0).getTitle();
+        String description = mockAdminProductionJpaRepository.findProductionsList(productionMap).get(0).getDescription();
+        String visible = mockAdminProductionJpaRepository.findProductionsList(productionMap).get(0).getVisible();
+
+        assertThat(idx).isEqualTo(productionList.get(0).getIdx());
+        assertThat(title).isEqualTo(productionList.get(0).getTitle());
+        assertThat(description).isEqualTo(productionList.get(0).getDescription());
+        assertThat(visible).isEqualTo(productionList.get(0).getVisible());
+    }
+
+    @Test
+    public void 프로덕션상세BDD조회테스트() throws Exception {
+
+        // given
+        CommonImageEntity commonImageEntity = CommonImageEntity.builder()
+                .idx(1)
+                .imageType("main")
+                .fileName("test.jpg")
+                .fileMask("test.jpg")
+                .filePath("/test/test.jpg")
+                .typeIdx(1)
+                .typeName("production")
+                .build();
+
+        List<CommonImageEntity> commonImageEntityList = new ArrayList<>();
+        commonImageEntityList.add(commonImageEntity);
+
+        AdminProductionEntity adminProductionEntity = builder().idx(1).commonImageEntityList(commonImageEntityList).build();
+
+        AdminProductionDTO adminProductionDTO = AdminProductionDTO.builder()
+                .idx(1)
+                .title("프로덕션 테스트")
+                .description("프로덕션 테스트")
+                .visible("Y")
+                .productionImage(ModelImageMapper.INSTANCE.toDtoList(commonImageEntityList))
+                .build();
+
+        given(mockAdminProductionJpaRepository.findOneProduction(adminProductionEntity)).willReturn(adminProductionDTO);
+
+        // when
+        Integer idx = mockAdminProductionJpaRepository.findOneProduction(adminProductionEntity).getIdx();
+        String title = mockAdminProductionJpaRepository.findOneProduction(adminProductionEntity).getTitle();
+        String description = mockAdminProductionJpaRepository.findOneProduction(adminProductionEntity).getDescription();
+        String visible = mockAdminProductionJpaRepository.findOneProduction(adminProductionEntity).getVisible();
+        String fileName = mockAdminProductionJpaRepository.findOneProduction(adminProductionEntity).getProductionImage().get(0).getFileName();
+        String fileMask = mockAdminProductionJpaRepository.findOneProduction(adminProductionEntity).getProductionImage().get(0).getFileMask();
+        String filePath = mockAdminProductionJpaRepository.findOneProduction(adminProductionEntity).getProductionImage().get(0).getFilePath();
+        String imageType = mockAdminProductionJpaRepository.findOneProduction(adminProductionEntity).getProductionImage().get(0).getImageType();
+        String typeName = mockAdminProductionJpaRepository.findOneProduction(adminProductionEntity).getProductionImage().get(0).getTypeName();
+
+        assertThat(idx).isEqualTo(1);
+        assertThat(title).isEqualTo("프로덕션 테스트");
+        assertThat(description).isEqualTo("프로덕션 테스트");
+        assertThat(visible).isEqualTo("Y");
+        assertThat(fileName).isEqualTo("test.jpg");
+        assertThat(fileMask).isEqualTo("test.jpg");
+        assertThat(filePath).isEqualTo("/test/test.jpg");
+        assertThat(imageType).isEqualTo("main");
+        assertThat(typeName).isEqualTo("production");
     }
 }
