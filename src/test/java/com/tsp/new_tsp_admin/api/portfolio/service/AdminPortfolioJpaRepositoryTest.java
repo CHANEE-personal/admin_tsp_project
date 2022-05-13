6a9c3ec35d3e550c813b7014b0f752f74d7b3f8a
CHO CHAN HEE <cksgml4424@gmail.com>
2022-05-13 16:27:48 +0900

    Admin 포트폴리오 리스트 조회 BDD test case 커밋

--- src/test/java/com/tsp/new_tsp_admin/api/portfolio/service/AdminPortfolioJpaRepositoryTest.java
+++ src/test/java/com/tsp/new_tsp_admin/api/portfolio/service/AdminPortfolioJpaRepositoryTest.java
@@ -1,9 +1,11 @@
 package com.tsp.new_tsp_admin.api.portfolio.service;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.tsp.new_tsp_admin.api.domain.common.CommonImageDTO;
 import com.tsp.new_tsp_admin.api.domain.model.AdminModelEntity;
 import com.tsp.new_tsp_admin.api.domain.portfolio.AdminPortFolioDTO;
 import com.tsp.new_tsp_admin.api.domain.portfolio.AdminPortFolioEntity;
+import com.tsp.new_tsp_admin.api.domain.portfolio.QAdminPortFolioEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,14 +20,18 @@ import org.springframework.test.context.TestPropertySource;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static com.tsp.new_tsp_admin.api.domain.portfolio.AdminPortFolioEntity.builder;
+import static com.tsp.new_tsp_admin.api.domain.portfolio.QAdminPortFolioEntity.adminPortFolioEntity;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.BDDMockito.given;
 
 @DataJpaTest
 @Transactional
@@ -39,7 +45,7 @@ class AdminPortfolioJpaRepositoryTest {
     private AdminPortfolioJpaRepository adminPortfolioJpaRepository;
 
     @Mock
-    private AdminPortfolioJpaRepository mockPortfolioJpaRepository;
+    private AdminPortfolioJpaRepository mockAdminPortfolioJpaRepository;
 
     @Autowired
     private EntityManager em;
@@ -79,5 +85,44 @@ class AdminPortfolioJpaRepositoryTest {
                 () -> {
                     assertThat(portfolioInfo.getDescription()).isEqualTo("포트폴리오 테스트");
                 });
+    }
+
+    @Test
+    public void 포트폴리오BDD조회테스트() throws Exception {
+
+        // given
+        ConcurrentHashMap<String, Object> portfolioMap = new ConcurrentHashMap<>();
+        portfolioMap.put("jpaStartPage", 1);
+        portfolioMap.put("size", 3);
+
+        CommonImageDTO commonImageDTO = CommonImageDTO.builder()
+                .idx(1)
+                .imageType("main")
+                .fileName("test.jpg")
+                .fileMask("test.jpg")
+                .filePath("/test/test.jpg")
+                .typeIdx(1)
+                .typeName("portfolio")
+                .build();
+
+        List<CommonImageDTO> commonImageDtoList = new ArrayList<>();
+        commonImageDtoList.add(commonImageDTO);
+
+        List<AdminPortFolioDTO> portfolioList = new ArrayList<>();
+        portfolioList.add(AdminPortFolioDTO.builder().idx(1)
+                .title("포트폴리오 테스트").description("포트폴리오 테스트")
+                .portfolioImage(commonImageDtoList).build());
+
+        given(mockAdminPortfolioJpaRepository.findPortfoliosList(portfolioMap)).willReturn(portfolioList);
+
+        // when
+        Integer idx = mockAdminPortfolioJpaRepository.findPortfoliosList(portfolioMap).get(0).getIdx();
+        String title = mockAdminPortfolioJpaRepository.findPortfoliosList(portfolioMap).get(0).getTitle();
+        String description = mockAdminPortfolioJpaRepository.findPortfoliosList(portfolioMap).get(0).getDescription();
+
+        // then
+        assertThat(idx).isEqualTo(portfolioList.get(0).getIdx());
+        assertThat(title).isEqualTo(portfolioList.get(0).getTitle());
+        assertThat(description).isEqualTo(portfolioList.get(0).getDescription());
     }
 }
