7745112913c458cb2bf8956563741f00b7b6c748
CHO CHAN HEE <cksgml4424@gmail.com>
2022-05-28 21:18:49 +0900

    PasswordEncoderFactories.createDelegatingPasswordEncoder()
    test case 수정

--- src/test/java/com/tsp/new_tsp_admin/api/jwt/PasswordEncoderTest.java
+++ src/test/java/com/tsp/new_tsp_admin/api/jwt/PasswordEncoderTest.java
@@ -1,16 +1,23 @@
 package com.tsp.new_tsp_admin.api.jwt;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.*;
 import org.springframework.test.context.TestPropertySource;
 
 import javax.transaction.Transactional;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,6 +26,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class PasswordEncoderTest {
 
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
     @Test
     public void delegatingPasswordEncoding테스트() {
 
@@ -26,7 +40,6 @@ public class PasswordEncoderTest {
         String password = "pass1234";
 
         // when
-        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         String encodedPassword = passwordEncoder.encode(password);
 
         // then
@@ -35,8 +48,66 @@ public class PasswordEncoderTest {
 
     @Test
     public void defaultDelegatingPasswordEncoder() {
-        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         String encodedPassword = passwordEncoder.encode("pass1234");
         assertThat(encodedPassword.startsWith("{bcrypt}")).isTrue();
+    }
+
+    @Test
+    public void match() {
+        String password = "password";
+        String encPassword1 = "{pbkdf2}7a07c208fc2a407fb89cc3b6effb1b759da575a85f65dda9cd426f1ad14b56e6afaeeea6f9269569";
+        String encPassword2 = "{bcrypt}$2a$10$Ot44NE6k1kO5bfNHTP0m8ejdpGr8ooHGT90lOD2/LpGIzfiS3p6oq";
+        String encPassword3 = "{sha256}fcef9e3f82af42d9059e74a95c633fe99b7aba1c4bfb9ac1cae31dd1b67060da933776fee8baec8f";
+
+        assertThat(passwordEncoder.matches(password, encPassword1)).isTrue();
+        assertThat(passwordEncoder.matches(password, encPassword2)).isTrue();
+        assertThat(passwordEncoder.matches(password, encPassword3)).isTrue();
+    }
+
+    @Test
+    public void 사용한_비밀번호변환기_접두사가없으면오류발생() {
+        String password = "password";
+        String encPassword1 = "7a07c208fc2a407fb89cc3b6effb1b759da575a85f65dda9cd426f1ad14b56e6afaeeea6f9269569";   // pbkdf2
+        String encPassword2 = "$2a$10$Ot44NE6k1kO5bfNHTP0m8ejdpGr8ooHGT90lOD2/LpGIzfiS3p6oq";                       // bcrypt
+        String encPassword3 = "fcef9e3f82af42d9059e74a95c633fe99b7aba1c4bfb9ac1cae31dd1b67060da933776fee8baec8f";   // sha256
+
+        assertThrows(IllegalArgumentException.class, () -> passwordEncoder.matches(password, encPassword1));
+        assertThrows(IllegalArgumentException.class, () -> passwordEncoder.matches(password, encPassword2));
+        assertThrows(IllegalArgumentException.class, () -> passwordEncoder.matches(password, encPassword3));
+    }
+
+    @Test
+    public void 암호변환기ID가_없는경우는_다음과같이() {
+        String password = "password";
+        String encPassword = "$2a$10$Ot44NE6k1kO5bfNHTP0m8ejdpGr8ooHGT90lOD2/LpGIzfiS3p6oq";                       // bcrypt
+
+        DelegatingPasswordEncoder delegatingPasswordEncoder = (DelegatingPasswordEncoder) PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        delegatingPasswordEncoder.setDefaultPasswordEncoderForMatches(new BCryptPasswordEncoder());
+        assertThat(delegatingPasswordEncoder.matches(password, encPassword)).isTrue();
+    }
+
+    @Test
+    public void customDelegatingPasswordEncoder() {
+
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        String idForEncode = "bcrypt";
+        encoders.put(idForEncode, new BCryptPasswordEncoder());
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
+        encoders.put("sha256", new StandardPasswordEncoder());
+
+        passwordEncoder = new DelegatingPasswordEncoder(idForEncode, encoders);
+
+        String password = "password";
+        String encPassword = passwordEncoder.encode(password);
+        assertThat(passwordEncoder.matches(password, encPassword)).isTrue();
+
+        String encPassword1 = "{pbkdf2}7a07c208fc2a407fb89cc3b6effb1b759da575a85f65dda9cd426f1ad14b56e6afaeeea6f9269569";
+        String encPassword2 = "{bcrypt}$2a$10$Ot44NE6k1kO5bfNHTP0m8ejdpGr8ooHGT90lOD2/LpGIzfiS3p6oq";
+        String encPassword3 = "{sha256}fcef9e3f82af42d9059e74a95c633fe99b7aba1c4bfb9ac1cae31dd1b67060da933776fee8baec8f";
+
+        assertThat(passwordEncoder.matches(password, encPassword1)).isTrue();
+        assertThat(passwordEncoder.matches(password, encPassword2)).isTrue();
+        assertThat(passwordEncoder.matches(password, encPassword3)).isTrue();
     }
 }
