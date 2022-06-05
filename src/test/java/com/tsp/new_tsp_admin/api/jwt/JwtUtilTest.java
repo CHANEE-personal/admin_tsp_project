dc10748255aa7d6f73628ac7a5ace19ce94ec183
CHO CHAN HEE <cksgml4424@gmail.com>
2022-06-05 22:16:45 +0900

    jwtUtilTest displayname 추가

--- src/test/java/com/tsp/new_tsp_admin/api/jwt/JwtUtilTest.java
+++ src/test/java/com/tsp/new_tsp_admin/api/jwt/JwtUtilTest.java
@@ -2,6 +2,7 @@ package com.tsp.new_tsp_admin.api.jwt;
 
 import com.tsp.new_tsp_admin.api.domain.user.AuthenticationRequest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -42,17 +43,20 @@ class JwtUtilTest {
     }
 
     @Test
+    @DisplayName("토큰 만료 테스트")
     public void isTokenExpiredTest() throws Exception {
         String token = jwtUtil.generateToken(userDetails);
         assertFalse(jwtUtil.isTokenExpired(token));
     }
 
     @Test
+    @DisplayName("토큰 생성 테스트")
     public void generateTokenTest() throws Exception {
         assertNotNull(jwtUtil.generateToken(userDetails));
     }
 
     @Test
+    @DisplayName("리프레시 토큰 생성 테스트")
     public void generateRefreshTokenTest() throws Exception {
         assertNotNull(jwtUtil.generateToken(userDetails));
     }
@@ -64,6 +68,7 @@ class JwtUtilTest {
     }
 
     @Test
+    @DisplayName("토큰 유효성 테스트")
     public void validateTokenTest() throws Exception {
         String token = jwtUtil.generateToken(userDetails);
         assertTrue(jwtUtil.validateToken(token, userDetails));
