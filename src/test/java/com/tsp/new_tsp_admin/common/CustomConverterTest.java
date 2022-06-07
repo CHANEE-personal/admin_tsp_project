9efae653e2cdbb72fe5dd1c051e77a1acdb79fb8
CHO CHAN HEE <cksgml4424@gmail.com>
2022-06-08 08:19:41 +0900

    customerConverter test case commit

---
+++ src/test/java/com/tsp/new_tsp_admin/common/CustomConverterTest.java
@@ -0,0 +1,21 @@
+package com.tsp.new_tsp_admin.common;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.properties")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CustomConverterTest {
+
+    
+
+}
