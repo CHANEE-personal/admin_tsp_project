c625698bd3c17b1b37f2353f581a7207680705a8
CHO CHAN HEE <cksgml4424@gmail.com>
2022-06-23 08:53:32 +0900

    junit production api test spring security 적용

--- src/test/java/com/tsp/new_tsp_admin/api/production/AdminProductionJpaControllerTest.java
+++ src/test/java/com/tsp/new_tsp_admin/api/production/AdminProductionJpaControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -22,6 +23,7 @@ import javax.transaction.Transactional;
 import static com.tsp.new_tsp_admin.api.domain.production.AdminProductionEntity.builder;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -59,6 +61,7 @@ class AdminProductionJpaControllerTest {
     public void setup() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(wac)
                 .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .apply(springSecurity())
                 .alwaysDo(print())
                 .build();
 
@@ -66,6 +69,7 @@ class AdminProductionJpaControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "ADMIN")
     @DisplayName("Admin 프로덕션 조회 테스트")
     public void 프로덕션조회Api테스트() throws Exception {
         mockMvc.perform(get("/api/jpa-production/lists"))
@@ -75,6 +79,16 @@ class AdminProductionJpaControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
+    @DisplayName("Admin 프로덕션 조회 권한 테스트")
+    public void 프로덕션조회Api권한테스트() throws Exception {
+        mockMvc.perform(get("/api/jpa-production/lists"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
     @DisplayName("Admin 프로덕션 상세 조회 테스트")
     public void 프로덕션상세조회Api테스트() throws Exception {
         mockMvc.perform(get("/api/jpa-production/1"))
@@ -86,6 +100,16 @@ class AdminProductionJpaControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
+    @DisplayName("Admin 프로덕션 상세 조회 권한 테스트")
+    public void 프로덕션상세조회Api권한테스트() throws Exception {
+        mockMvc.perform(get("/api/jpa-production/1"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
     @DisplayName("Admin 프로덕션 등록 테스트")
     public void 프로덕션등록Api테스트() throws Exception {
         mockMvc.perform(post("/api/jpa-production")
@@ -98,6 +122,18 @@ class AdminProductionJpaControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
+    @DisplayName("Admin 프로덕션 등록 권한 테스트")
+    public void 프로덕션등록Api권한테스트() throws Exception {
+        mockMvc.perform(post("/api/jpa-production")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(adminProductionEntity)))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
     @DisplayName("Admin 프로덕션 수정 테스트")
     public void 프로덕션수정Api테스트() throws Exception {
         em.persist(adminProductionEntity);
@@ -114,6 +150,22 @@ class AdminProductionJpaControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
+    @DisplayName("Admin 프로덕션 수정 권한 테스트")
+    public void 프로덕션수정Api권한테스트() throws Exception {
+        em.persist(adminProductionEntity);
+
+        adminProductionEntity = builder().idx(adminProductionEntity.getIdx()).title("테스트1").description("테스트1").build();
+
+        mockMvc.perform(put("/api/jpa-production/{idx}", adminProductionEntity.getIdx())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(adminProductionEntity)))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
     @DisplayName("Admin 프로덕션 삭제 테스트")
     public void 프로덕션삭제Api테스트() throws Exception {
         em.persist(adminProductionEntity);
@@ -123,5 +175,18 @@ class AdminProductionJpaControllerTest {
                         .content(objectMapper.writeValueAsString(adminProductionEntity)))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    @DisplayName("Admin 프로덕션 삭제 권한 테스트")
+    public void 프로덕션삭제Api권한테스트() throws Exception {
+        em.persist(adminProductionEntity);
+
+        mockMvc.perform(delete("/api/jpa-production/{idx}", adminProductionEntity.getIdx())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(adminProductionEntity)))
+                .andDo(print())
+                .andExpect(status().isForbidden());
     }
 }
