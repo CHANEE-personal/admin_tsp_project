f24057a8af0ff9f29e995a12f65b2a6e3707676a
CHO CHAN HEE <cksgml4424@gmail.com>
2022-08-25 15:20:14 +0900

    page @ApiModelProperty example 추가

--- src/main/java/com/tsp/new_tsp_admin/common/Page.java
+++ src/main/java/com/tsp/new_tsp_admin/common/Page.java
@@ -9,14 +9,14 @@ import lombok.Setter;
 @Setter
 @ApiModel
 public class Page {
-    @ApiModelProperty(value = "페이지 번호(0..N)")
+    @ApiModelProperty(value = "페이지 번호(0..N)", example = "1")
     private Integer page;
 
-    @ApiModelProperty(value = "페이지 크기", allowableValues = "range[0,100]")
+    @ApiModelProperty(value = "페이지 크기", allowableValues = "range[0,100]", example = "1")
     private Integer size;
 
     // 시작페이지
-    @ApiModelProperty(value = "시작페이지", hidden = true)
+    @ApiModelProperty(value = "시작페이지", hidden = true, example = "1")
     public Integer getStartPage() {
         return (page - 1) * size;
     }
