ac65beed332bfc0793530d7ce851f0552a12955c
CHO CHAN HEE <cksgml4424@gmail.com>
2022-08-31 14:10:38 +0900

    model schedule entity, dto, mapper commit #39

---
+++ src/main/java/com/tsp/new_tsp_admin/api/domain/model/schedule/AdminScheduleEntity.java
@@ -0,0 +1,55 @@
+package com.tsp.new_tsp_admin.api.domain.model.schedule;
+
+import com.tsp.new_tsp_admin.api.domain.common.NewCommonMappedClass;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+import java.util.Date;
+
+import static javax.persistence.GenerationType.IDENTITY;
+import static javax.persistence.TemporalType.TIMESTAMP;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tsp_model_schedule")
+public class AdminScheduleEntity extends NewCommonMappedClass {
+    @Transient
+    private Integer rnum;
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "idx")
+    private Integer idx;
+
+    @Column(name = "model_idx")
+    @ApiModelProperty(value = "모델 idx", required = true)
+    private Integer modelIdx;
+
+    @Column(name = "model_schedule")
+    @Lob
+    @NotEmpty(message = "모델 스케줄 입력은 필수입니다.")
+    private String modelSchedule;
+
+    @Column(name = "model_schedule_time")
+    @Temporal(value = TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotEmpty(message = "모델 스케줄 일정 입력은 필수입니다.")
+    private Date modelScheduleTime;
+
+    @Column(name = "visible")
+    @NotEmpty(message = "모델 스케줄 노출 여부 선택은 필수입니다.")
+    private String visible;
+}
---
+++ src/main/java/com/tsp/new_tsp_admin/api/model/mapper/schedule/ScheduleMapper.java
@@ -0,0 +1,26 @@
+package com.tsp.new_tsp_admin.api.model.mapper.schedule;
+
+import com.tsp.new_tsp_admin.api.domain.model.schedule.AdminScheduleDTO;
+import com.tsp.new_tsp_admin.api.domain.model.schedule.AdminScheduleEntity;
+import com.tsp.new_tsp_admin.api.model.mapper.agency.AgencyMapper;
+import com.tsp.new_tsp_admin.common.StructMapper;
+
+import java.util.List;
+
+import static org.mapstruct.factory.Mappers.getMapper;
+
+public interface ScheduleMapper extends StructMapper<AdminScheduleDTO, AdminScheduleEntity> {
+    ScheduleMapper INSTANCE = getMapper(ScheduleMapper.class);
+
+    @Override
+    AdminScheduleDTO toDto(AdminScheduleEntity entity);
+
+    @Override
+    AdminScheduleEntity toEntity(AdminScheduleDTO dto);
+
+    @Override
+    List<AdminScheduleDTO> toDtoList(List<AdminScheduleEntity> entityList);
+
+    @Override
+    List<AdminScheduleEntity> toEntityList(List<AdminScheduleDTO> dtoList);
+}
