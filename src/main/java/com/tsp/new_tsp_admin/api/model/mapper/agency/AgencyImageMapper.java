15a4b8694cfae5062564f49e0b0889659d25f441
CHO CHAN HEE <cksgml4424@gmail.com>
2022-09-16 09:49:29 +0900

    mapper annotation 추가

--- src/main/java/com/tsp/new_tsp_admin/api/model/mapper/agency/AgencyImageMapper.java
+++ src/main/java/com/tsp/new_tsp_admin/api/model/mapper/agency/AgencyImageMapper.java
@@ -3,11 +3,13 @@ package com.tsp.new_tsp_admin.api.model.mapper.agency;
 import com.tsp.new_tsp_admin.api.domain.common.CommonImageDTO;
 import com.tsp.new_tsp_admin.api.domain.common.CommonImageEntity;
 import com.tsp.new_tsp_admin.common.StructMapper;
+import org.mapstruct.Mapper;
 
 import java.util.List;
 
 import static org.mapstruct.factory.Mappers.getMapper;
 
+@Mapper
 public interface AgencyImageMapper extends StructMapper<CommonImageDTO, CommonImageEntity> {
     AgencyImageMapper INSTANCE = getMapper(AgencyImageMapper.class);
 
