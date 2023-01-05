be69a4094fc65afc9feab669f61076108a48156a
CHO CHAN HEE <cksgml4424@gmail.com>
2023-01-05 15:38:15 +0900

    추천 검색어 관련 repository, sql 등 commit #105

---
+++ src/main/java/com/tsp/new_tsp_admin/api/domain/model/recommend/AdminRecommendEntity.java
@@ -0,0 +1,55 @@
+package com.tsp.new_tsp_admin.api.domain.model.recommend;
+
+import com.tsp.new_tsp_admin.api.domain.common.NewCommonMappedClass;
+import com.vladmihalcea.hibernate.type.json.JsonStringType;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+
+import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@EqualsAndHashCode(of = "idx", callSuper = false)
+@AllArgsConstructor
+@NoArgsConstructor
+@TypeDef(name = "json", typeClass = JsonStringType.class)
+@Table(name = "tsp_recommend")
+public class AdminRecommendEntity extends NewCommonMappedClass {
+    @Transient
+    private Integer rowNum;
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "idx")
+    private Long idx;
+
+    @Type(type = "json")
+    @Column(columnDefinition = "json", name = "recommend_keyword")
+    private List<String> recommendKeyword = new ArrayList<>();
+
+    public static AdminRecommendDTO toDto(AdminRecommendEntity entity) {
+        if (entity == null) return null;
+        return AdminRecommendDTO.builder()
+                .rowNum(entity.getRowNum())
+                .idx(entity.getIdx())
+                .recommendKeyword(entity.getRecommendKeyword())
+                .build();
+    }
+
+    public static List<AdminRecommendDTO> toDtoList(List<AdminRecommendEntity> entityList) {
+        if (entityList == null) return null;
+        return entityList.stream()
+                .map(AdminRecommendEntity::toDto)
+                .collect(Collectors.toList());
+    }
+}
